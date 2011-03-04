63bb01f81091a5c86d467312bb97575e52af6ec8
Donal Stewart <donal.stewart@bcs.org.uk>
2011-03-04 13:30:09 +0000

    Added user guide

--- src/java/org/demonsoft/spatialkappa/tools/Version.java
+++ src/java/org/demonsoft/spatialkappa/tools/Version.java
@@ -2,7 +2,7 @@ package org.demonsoft.spatialkappa.tools;
 
 public class Version {
 
-    public static final String VERSION = "0.9.0";
-    public static final String BUILD_NUMBER = "20101118";
+    public static final String VERSION = "1.0.0";
+    public static final String BUILD_NUMBER = "20110302";
     
 }
