04b159a25139a4f5c2960e0a72320429d1e644ff
Donal Stewart <donal.stewart@demonsoft.co.uk>
2012-05-17 00:00:00 +0100

    Added more PSD stage 1 acceptance tests.

--- test/acceptance/java/psd/SpatialTranslatorTest.java
+++ test/acceptance/java/psd/SpatialTranslatorTest.java
@@ -1,13 +1,11 @@
 package psd;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.demonsoft.spatialkappa.tools.SpatialTranslator;
-import org.junit.Before;
 import org.junit.Test;
 
 //TODO - extension to allow mixed rule and agent locations
@@ -16,23 +14,31 @@ public class SpatialTranslatorTest {
 
     private SpatialTranslator translator;
     
-    @Before
-    public void setUp() throws Exception {
-        translator = new SpatialTranslator("%compartment: cytosol [4]\n");
-    }
-    
     @Test
-    public void testSpatialTranslatorFile() throws Exception {
-        try {
-            new SpatialTranslator((File) null);
-            fail("null should have failed");
-        }
-        catch (NullPointerException ex) {
-            // Expected exception
-        }
-        
+    public void testTest1_1_dimerisationWithinIndividualVoxel() throws Exception {
         translator = new SpatialTranslator(new File(TEST_DATA_DIRECTORY, "test-1-1-input.ka"));
         String testOutput = FileUtils.readFileToString(new File(TEST_DATA_DIRECTORY, "test-1-1-output.ka"));
+        assertEquals(testOutput, translator.translateToKappa());
+    }
+
+    @Test
+    public void testTest1_2_dimerisationAcrossReactionSurfaceWithinSingleCompartment() throws Exception {
+        translator = new SpatialTranslator(new File(TEST_DATA_DIRECTORY, "test-1-2-input.ka"));
+        String testOutput = FileUtils.readFileToString(new File(TEST_DATA_DIRECTORY, "test-1-2-output.ka"));
+        assertEquals(testOutput, translator.translateToKappa());
+    }
+
+    @Test
+    public void testTest1_3_dimerisationAcrossReactionSurfaceBetweenCompartments() throws Exception {
+        translator = new SpatialTranslator(new File(TEST_DATA_DIRECTORY, "test-1-3-input.ka"));
+        String testOutput = FileUtils.readFileToString(new File(TEST_DATA_DIRECTORY, "test-1-3-output.ka"));
+        assertEquals(testOutput, translator.translateToKappa());
+    }
+
+    @Test
+    public void testTest1_4_initialComplexesWithinSameCompartment() throws Exception {
+        translator = new SpatialTranslator(new File(TEST_DATA_DIRECTORY, "test-1-4-input.ka"));
+        String testOutput = FileUtils.readFileToString(new File(TEST_DATA_DIRECTORY, "test-1-4-output.ka"));
         assertEquals(testOutput, translator.translateToKappa());
     }
 
